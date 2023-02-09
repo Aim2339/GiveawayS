5b6354065e898dd69e04aaf865ea6978477d5d87
Aim <91464959+Aim2339@users.noreply.github.com>
2023-02-09 06:35:08 +0000

    Duplicate code

--- events/discord/messageCreate.js
+++ events/discord/messageCreate.js
@@ -1,13 +1,38 @@
-module.exports = (client, interaction) => {
-  if (interaction.isCommand()) {
-    const command = client.interactions.get(interaction.commandName);
+const messageCount = require("../../schema/messageCount");
 
-    if (!command)
-      return interaction.reply({
-        content: ":x: Something Went Wrong | Perhaps command not registered?",
-        ephemeral: true,
-      });
+module.exports = (client, message) => {
+  // Ignore messages sent by bots
+  if (message.author.bot) return;
 
-    command.run(client, interaction);
-  }
+  // check if the document already exist
+  messageCount.findOne(
+    { userId: message.author.id, guildId: message.guild.id },
+    function (err, doc) {
+      if (err) {
+        return;
+      }
+      if (!doc) {
+        // create new document with count = 1
+        messageCount.create(
+          { userId: message.author.id, guildId: message.guild.id, count: 1 },
+          function (err) {
+            if (err) {
+              return;
+            }
+          }
+        );
+      } else {
+        // increment the count by 1
+        messageCount.findOneAndUpdate(
+          { userId: message.author.id, guildId: message.guild.id },
+          { $inc: { count: 1 } },
+          function (err) {
+            if (err) {
+              return;
+            }
+          }
+        );
+      }
+    }
+  );
 };
